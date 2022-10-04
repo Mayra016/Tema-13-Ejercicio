1fb0eb40929f5cb8b19e3e176c92fe924986de0b
Mayra016 <111470957+Mayra016@users.noreply.github.com>
2022-10-04 20:33:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,8 @@ boton.addEventListener("click", () => {
     alert("Haz hecho click en el botón");
 })
 
-//Crea un servidor
-const http = require("http");
-const host = 'localhost';
-const port = 8000;
-const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end("My first server!");
-};
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
-
 //Botón JQuerry
 const jquerry-btn = document.querySelector("#jquerry-btn");
-jquerry-btn.addEventListener("jquerry", () => {
+$("jquerry-btn").click(function() {
     console.log("Hola, estoy utilizando jQuery")
 })
